cb814b79fec92082a1f0a16891899b94c32a55e6
polad9ev <poladevn@mail.ru>
2019-06-17 17:46:11 +0300

    Сверстана секция portfolio и сделан слайдер (slick). C Адаптивом

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,11 +1,28 @@
 $(function() {
-  $('.slider').slick({
+  $(".slider").slick({
     infinite: true,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 5000,
-    prevArrow: $('.arrows__left'),
-    nextArrow: $('.arrows__right')
+    prevArrow: $(".arrows__left"),
+    nextArrow: $(".arrows__right"),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          autoplay: false,
+          slidesToShow: 1,
+          slidesToScroll: 1 
+        }
+      }
+    ]
   });
 });
